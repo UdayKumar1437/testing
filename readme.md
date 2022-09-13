4557f01c35f043ff5210adaa8572c1e236e28c1d
UdayKumar1437 <92321145+UdayKumar1437@users.noreply.github.com>
2022-09-13 21:28:15 +0530

    uday

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Testing Purpose
+
+**Uday Kumar**
---
+++ tubers/webpages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'webpages/home.html')
+
+def about(request):
+    return render(request,'webpages/about.html')
+
+
+def services(request):
+    return render(request,'webpages/services.html')
+
+def contact(request):
+    return render(request,'webpages/contact.html')
